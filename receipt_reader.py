b4d9245a40617a5ca46aca0c17ef405c2741c9dd
Nick <nsenador1@gmail.com>
2019-01-23 12:49:03 -0500

    Parse Publix HTML to return date, items, and total

--- receipt_reader.py
+++ receipt_reader.py
@@ -1,18 +1,52 @@
-from bs4 import BeautifulSoup
+import re
 from fuzzywuzzy import fuzz
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/get-receipt-info", methods=["GET"])
-def getReceiptInfo():
+def get_receipt_info():
     receipt = request.get_data()
-    soup = BeautifulSoup(receipt, "html.parser")
-    items = list(soup.p.stripped_strings)
-    receipt_info = []
-    for item in items[0].splitlines():
-        print(item.lower())
-        receipt_info.append(find_closest_string(item.lower()))
-    return jsonify({"items": receipt_info})
+    store = get_store(receipt)
+    date = get_date(receipt)
+    items = get_items(receipt)
+    total = get_total(receipt)
+    return jsonify(date=date, items=items, store=store, total=total)
+
+
+def get_store(receipt):
+    return""
+
+
+def get_date(receipt):
+    return re.findall(r'\d\d\/\d\d\/\d\d\d\d', receipt)[0]
+
+
+def get_total(receipt):
+    return re.findall(r'\$\d?\d\.\d\d', receipt)[0]
+
+
+def get_items(receipt):
+    # get all the lines with items
+    line_items = re.findall(r'<pre style=\"text-align:center\">(?:[A-Z]|\d|&nbsp;|\s|\.|t|%)*<u></u><u></u></pre>',
+                            receipt)
+    items = []
+    # get the items for each line
+    for line_item in line_items:
+        print(line_item)
+        item = re.findall(r'(?:[A-Z]|\d)(?:[A-Z]|\s|\d|\.|%)+', line_item)
+        # items with price
+        if len(item) == 1:
+            items.append(item)
+        # items without price
+        elif len(item) == 3:
+            items.append(item[:2])
+    return translate_items(items)
+
+
+# work will be done here
+def translate_items(items):
+    return items
+
 
 def find_closest_string(string):
     products = open("resources/Products")
@@ -24,6 +58,7 @@ def find_closest_string(string):
             closest_ratio = distance
             closest_string = line.lower()
     return closest_string
+
 
 if __name__ == "__main__":
     app.run(debug=True)

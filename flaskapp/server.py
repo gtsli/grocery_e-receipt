62b8ad4517d0d1908879cd39228ce9252ae4b145
sarah <gtsli469@gmail.com>
2019-04-17 20:47:15 -0400

    add api endpoints for edit distance (for user inputted strings)

--- flaskapp/server.py
+++ flaskapp/server.py
@@ -17,6 +17,10 @@ from keras.utils import np_utils
 from keras.utils import to_categorical
 from sklearn import preprocessing
 
+# edit distance imports
+from fuzzywuzzy import fuzz
+from weighted_levenshtein import lev, osa, dam_lev
+
 app = Flask(__name__)
 with open("receipt_outputs/ex_output.json") as in_json:
 	receipt_titles = json.load(in_json)
@@ -31,11 +35,13 @@ def reload():
 @app.route("/retrieve", methods=['POST', 'GET'])
 def retrieve():
 	searched = request.form['searched_receipt']
-	output = lstm_output(searched, True)
-	print ('output = ', output)
+	lstm_output, edit_distance_output = predict(searched, True)
+	print('lstm_output = ', lstm_output)
+	print('edit_distance_output = ', edit_distance_output)
 	return render_template("ui.html", manual_search=True,
 										receipt_titles=searched,
-										lstm_output=output)
+										lstm_output=lstm_output,
+										edit_distance_output=edit_distance_output)
 
 @app.route("/index")
 def load_blank():
@@ -55,8 +61,8 @@ def upload_file():
 
 
 
-@app.route("/LSTM", methods=['POST', 'GET'])
-def lstm_output(searched=None, internal_call=False):
+@app.route("/predict", methods=['POST', 'GET'])
+def predict(searched=None, internal_call=False):
 	'''
 	important variables:
 	tokenizer - tokenizer for tokenizing text
@@ -64,33 +70,129 @@ def lstm_output(searched=None, internal_call=False):
 	le - label encoder to decode output
 	'''
 
+	# lstm
 	K.clear_session()
 	lstm_path = "../approaches/LSTM/"
 	with open(lstm_path+"pickled/tokenizer.pickle", 'rb') as handle:
-	    tokenizer = pickle.load(handle)
+		tokenizer = pickle.load(handle)
 	lstm_model = load_model(lstm_path+"models/lstm_50k_epochs_5.h5")
 	le = preprocessing.LabelEncoder()
 	le.classes_ = np.load(lstm_path+"pickled/labelencoder_classes.npy")
 	tokenizer.oov_token = None
 
+	# edit distance
+	with open('../approaches/edit_distance/cleaned_bucket_data.json', encoding="ASCII") as f:
+		data = json.load(f)
+
 	## if internal call ##
 	if internal_call:
+		# lstm
 		encoded_x = tokenizer.texts_to_sequences([searched])
 		padded = pad_sequences(encoded_x, 25)
-		preds = lstm_model.predict(padded)
-		pred_labels = [[np.argmax(x)] for x in preds]
-		preds = le.inverse_transform(pred_labels)
-		return preds
+		lstm_preds = lstm_model.predict(padded)
+		pred_labels = [[np.argmax(x)] for x in lstm_preds]
+		lstm_preds = le.inverse_transform(pred_labels)
+
+		#=========================================================
+
+		# edit distance
+		# print("searched: " + searched)
+		# find first letter of every word in the string
+		words = searched.split()
+		# print("words: " + str(words))
+		letters = [word[0] for word in words]
+		# print("letters: " + str(letters))
+		# get corresponding buckets 
+		first_letter = searched[0]
+		products = []
+		for bucket in data:
+			# print("bucket " + str(bucket))
+			# print("first_letter " + str(first_letter.lower()))
+			# print("bucket[0][0] " + str(bucket[0][0].lower()))
+			if bucket[0][0].lower() == first_letter.lower():
+				print("match!")
+				products += bucket
+
+		# remove non-ascii characters
+		cleaned_products = []
+		# print("products " + str(products))
+		for entry in products:
+			cleaned_entry = ""
+			for character in entry:
+				if ord(character) <= 128:
+					cleaned_entry += character
+			cleaned_products.append(cleaned_entry)
+		# print("cleaned_products " + str(cleaned_products))
+
+		insert_costs = np.full(128, .3, dtype=np.float64)  # make an array of all 1's of size 128, the number of ASCII characters
+		transpose_costs = np.full((128, 128), .7, dtype=np.float64)
+		delete_costs = np.full(128, 1.2, dtype=np.float64)
+
+		closest_distance = 999999
+		closest_string = None
+
+		for line in cleaned_products:
+			# print("line: " + str(line))
+			distance = osa(searched.lower(), line.lower(), insert_costs=insert_costs, transpose_costs=transpose_costs, delete_costs=delete_costs)
+			if closest_distance is None or distance < closest_distance:
+				closest_distance = distance
+				closest_string = line.lower()
+
+		print("closest_string: " + str(closest_string))
+		edit_distance_pred = closest_string
+		return (lstm_preds, edit_distance_pred)
 	else:
 		encoded_x = tokenizer.texts_to_sequences(receipt_titles)
 		padded = pad_sequences(encoded_x, 25)
-		preds = lstm_model.predict(padded)
-		pred_labels = [[np.argmax(x)] for x in preds]
-		preds = le.inverse_transform(pred_labels)
+		lstm_preds = lstm_model.predict(padded)
+		pred_labels = [[np.argmax(x)] for x in lstm_preds]
+		lstm_preds = le.inverse_transform(pred_labels)
+
+		#=========================================================
+
+		# edit distance
+		titles = json.load(receipt_titles)
+		edit_distance_preds = []
+		for title in titles:
+			# find first letter of every word in the string
+			words = searched.split()
+			letters = [word[0] for word in words]
+
+			# get corresponding buckets 
+			first_letter = searched[0]
+			products = []
+			for bucket in data:
+				if bucket[0][0] == first_letter:
+					products += bucket
+
+			# remove non-ascii characters
+			cleaned_products = []
+			for entry in products:
+				cleaned_entry = ""
+				for character in entry:
+					if ord(character) <= 128:
+						cleaned_entry += character
+				cleaned_products.append(cleaned_entry)
+
+			insert_costs = np.full(128, .3, dtype=np.float64)  # make an array of all 1's of size 128, the number of ASCII characters
+			transpose_costs = np.full((128, 128), .7, dtype=np.float64)
+			delete_costs = np.full(128, 1.2, dtype=np.float64)
+
+			closest_distance = 999999
+			closest_string = None
+
+			for line in cleaned_products:
+				distance = osa(string.lower(), line.lower(), insert_costs=insert_costs, transpose_costs=transpose_costs, delete_costs=delete_costs)
+				if closest_distance is None or distance < closest_distance:
+					closest_distance = distance
+					closest_string = line.lower()
+
+			edit_distance_preds.append(closest_string)
 
 		return render_template("ui.html", manual_search=False,
 											receipt_titles=receipt_titles,
-											lstm_output=preds)
+											lstm_output=lstm_preds,
+											edit_distance_ouput=edit_distance_preds)
 
 
 
